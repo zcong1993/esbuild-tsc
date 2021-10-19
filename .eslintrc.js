624857b8ec118b3e70a6443fd899b913ee0df3ea
zcong1993 <zhangcong1992@gmail.com>
2021-10-19 16:46:27 +0800

    ci: config ci

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     // '@typescript-eslint/no-use-before-define': 'off',
-    // '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
